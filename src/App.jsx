8df9a91aa5f69464ad403c1955d120b7ce188c84
Santiago Blachere <blachere.asantiago@gmail.com>
2024-10-04 15:59:43 -0300

    feat: fetching pokemons

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,40 @@
 import { useEffect, useState } from 'react'
-
+import './styles/styles.css'
 function App() {
 const [pokemons, setPokemons] = useState([])
+
+const getRandomId = () => {
+  return Math.floor(Math.random() * 1000)
+}
 const pokemon = async() => {
     try {
-     const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=10')
-     if (!response.ok) {
-       throw new Error('Fetch failed')
+     const pokemonPromises = [];
+     for (let index = 0; index < 10; index++) {
+      pokemonPromises.push(fetch(`https://pokeapi.co/api/v2/pokemon/${getRandomId()}`).then(res => res.json()))
      }
-     const data = await response.json()
-     return data
+     const pokemons = await Promise.all(pokemonPromises)
+     setPokemons(pokemons)
     } catch (error) {
-     console.log(error)
+      console.log(error)
     }
-} 
+  }
 useEffect(() => {
-  pokemon().then(data => {
-    setPokemons(data)
-  })
-}, [])
-
-console.log(pokemons)
-
-
+  pokemon()
+},[])
 
   return (
     <>
       <h1>COOOL MEMORY GAME HAH!</h1>
+      <button onClick={() => pokemon()}>POKEMONINS</button>
+      <h2>START PLAYING BY CLICKING ON A POKEMON!</h2>
+      <div className='pokemons'>
+        {pokemons.map(pokemon => (
+          <div key={pokemon.id} >
+          <div>{pokemon.name}</div>
+          <img src={pokemon.sprites.front_default} alt="pokemon" />
+          </div>
+        ))}
+      </div>
     </>
   )
 }
